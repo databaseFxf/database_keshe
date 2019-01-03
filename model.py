cbc24d46beb921ffccbaaeee08fa5e72bd482556
zhandong <1468767640@qq.com>
2019-01-03 17:51:06 +0800

    change model

--- model.py
+++ model.py
@@ -24,4 +24,55 @@ class UserEvaluation(Model):
     evaluation_time = TimeField()
 
 
+class UserComment(Model):
+    __table__ = "user_comments"
+    id = IntegerField(primary_key=True)
+    user_id = IntegerField()
+    movie_id = IntegerField()
+    content = TextField()
+    comment_time = TimeField()
+    tag = StringField(length=20)
 
+
+class Celebrate(Model):
+    __table__ = "celebrates"
+    id = IntegerField(primary_key=True)
+    celebrate_name = StringField(20)
+    celebrate_sex = StringField(5)
+    celebrate_constellation = StringField(20)
+    celebrate_birthday = TimeField()
+    celebrate_birthplace = StringField(50)
+    celebrate_professions = StringField(20)
+    celebrate_icon_url = StringField(50)
+
+
+class CelebrateMovie(Model):
+    __table__ = "celebrate_movies"
+    id = IntegerField(primary_key=True)
+    movie_id = StringField(length=10)
+    role = StringField(length=10)
+    this_profession = StringField(length=20)
+
+
+class Movie(Model):
+    __table__ = "movies"
+    id = IntegerField(primary_key=True)
+    movie_name = StringField(length=30)
+    movie_type = StringField(length=20)
+    movie_production = StringField(length=20)
+    movie_language = StringField(length=20)
+    movie_release_date = StringField(length=30)
+    movie_length = StringField(length=10)
+    movie_aliases = StringField(length=100)
+    IMDB_link = StringField(length=50)
+    movie_synopsis = TextField()
+    movie_poster_url = StringField(50)
+
+
+class MovieSource(Model):
+    __table__ = "movie_sources"
+    id = IntegerField(primary_key=True)
+    movie_id = IntegerField()
+    source_name = StringField(length=20)
+    source_url = StringField(length=50)
+    source_information = StringField(length=20)
